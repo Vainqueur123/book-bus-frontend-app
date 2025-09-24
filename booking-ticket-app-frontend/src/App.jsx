1359bcbe616092df5593038f34f55096982552e6
Gady-Hirwa-Ntwali <hirwagad767@gmail.com>
2025-09-24 22:53:58 +0200

    add supabase data in system

--- booking-ticket-app-frontend/src/App.jsx
+++ booking-ticket-app-frontend/src/App.jsx
@@ -1,18 +1,16 @@
-import BookingPage from "./welcome/components/BookingPage"
-import Settings from "./welcome/components/Setting"
-import Welcoming from "./welcome/components/welcomimg"
+import { Routes, Route } from "react-router-dom";
+import BookingPage from "./welcome/components/BookingPage";
+import Settings from "./welcome/components/Setting";
+import Welcoming from "./welcome/components/welcoming";
 
 function App() {
-  
-
   return (
-    <>
-    <Welcoming />
-    <BookingPage/>
-    <Settings/>
-    </>
-    
-  )
+    <Routes>
+      <Route path="/" element={<Welcoming />} />
+      <Route path="/booking" element={<BookingPage />} />
+      <Route path="/settings" element={<Settings />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
