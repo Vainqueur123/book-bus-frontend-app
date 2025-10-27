d69ae5ec3bfd4260d0432ee707632f0028b4eec7
Rumanzi <ndamukundavainqueur@gmail.com>
2025-10-27 21:10:05 +0200

    making seats page

--- booking-ticket-app-frontend/src/App.jsx
+++ booking-ticket-app-frontend/src/App.jsx
@@ -2,13 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import BookingPage from './welcome/components/BookingPage';
 import Settings from './welcome/components/Setting';
 import Welcoming from './welcome/components/welcoming';
+import BookingConfirm from './welcome/components/BookingConfirm';
+
 
 function App() {
   return (
+    
     <Routes>
       <Route path="/" element={<Welcoming />} />
       <Route path="/booking" element={<BookingPage />} />
+      
       <Route path="/settings" element={<Settings />} />
+      <Route path="/seats" element={<BookingConfirm />} />
     </Routes>
   );
 }
